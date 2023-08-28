26a541b66a136dd466670413cdbed9f0c67e636a
Austin Burgard <awburgard@gmail.com>
2023-08-28 17:23:13 -0500

    Create useSessionStorage.ts

---
+++ useSessionStorage.ts
@@ -0,0 +1,32 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+
+export default function useSessionStorage<T>(
+  key: string,
+  initialValue: T
+): [T, Dispatch<SetStateAction<T>>] {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    if (typeof window === 'undefined') {
+      return initialValue;
+    }
+    try {
+      const item = window.sessionStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  });
+
+  const setValue: (value: any) => void = (value: any) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      if (typeof window !== 'undefined') {
+        window.sessionStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
